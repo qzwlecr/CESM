2a3f771d8e5680345bd9f9d17bd140091292d72c
Lichen Zhang <qzwlecr@gmail.com>
2019-01-24 22:21:37 +0800

    fix: ntask number and compile para.

--- exp1.sh
+++ exp1.sh
@@ -4,7 +4,7 @@ create_newcase -case EXP1 -res 0.47x0.63_gx1v6 -compset E1850CN -mach cluster ||
 cd EXP1
 ./xmlchange -file env_run.xml -id DIN_LOC_ROOT -val /home/gpu_ubuntu/zhanglichen/asc2019/cesm/inputdata/inputdata_EXP1
 ./xmlchange -file env_run.xml -id DOCN_SOM_FILENAME -val pop_frc.gx1v6.091112.nc
-./xmlchange -file env_run.xml -id STOP_N -val 1
+./xmlchange -file env_run.xml -id STOP_N -val 5
 ./xmlchange -file env_run.xml -id STOP_OPTION -val ndays
 ./cesm_setup || exit 2
 ./EXP1.build || exit 3
