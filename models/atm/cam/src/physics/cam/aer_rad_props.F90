e3be5cc47bf18349e40d71680121acfd13e62268
GengYu Rao <zouyoo@outlook.com>
2019-01-29 13:29:40 +0800

    revert models/atm/cam/src/physics/cam/aer_rad_props.F90

--- models/atm/cam/src/physics/cam/aer_rad_props.F90
+++ models/atm/cam/src/physics/cam/aer_rad_props.F90
@@ -489,9 +489,9 @@ subroutine get_hygro_rad_props(ncol, krh, wrh, mass, ext, ssa, asm, &
                       - wrh(icol,ilev)  * asm(krh(icol,ilev),  iswband)
   !ASC todo cuda 
             tau    (icol, ilev, iswband) = mass(icol, ilev) * ext1  !这个地方要改成累乘
-            tau_w  (icol, ilev, iswband) = tau    (icol, ilev, iswband) * ssa1 !每次在之前的基础上再乘，，
-            tau_w_g(icol, ilev, iswband) = tau_w  (icol, ilev, iswband) * asm1
-            tau_w_f(icol, ilev, iswband) = tau_w_g(icol, ilev, iswband) * asm1
+            tau_w  (icol, ilev, iswband) = mass(icol, ilev) * ext1 * ssa1
+            tau_w_g(icol, ilev, iswband) = mass(icol, ilev) * ext1 * ssa1 * asm1
+            tau_w_f(icol, ilev, iswband) = mass(icol, ilev) * ext1 * ssa1 * asm1 * asm1
          enddo
       enddo
    enddo
