1738df8db379825c894aa1ac5d56afa2712e75a1
GengYu Rao <zouyoo@outlook.com>
2019-02-13 09:20:08 +0800

    remove old *.cu

--- models/atm/cam/bld/cuda-build.sh
+++ models/atm/cam/bld/cuda-build.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
+INTEL='/media/rgy/linux-file/intel_parallel_studio/'
 cd /media/rgy/win-file/document/computer/HPC/cesm/CESM/models/atm/cam/bld/
-echo "cuda!"
-nvcc   -ccbin gcc-7 -c ../src/utils/fft99_cuda.cu  -I/usr/local/cuda/inc -L/usr/local/cuda/lib -lcufft -o fft99_cuda.o
-#runtime as static --cudart static
-nvcc   -ccbin gcc-7  -c ../src/physics/cam/aer_rad_props_cuda.cu -o aer_rad_props_cuda.o  || exit 2
-g++  -c ../src/physics/cam/zm_conv_cuda.cpp -o zm_conv_cuda.o -O3  -Ofast || exit 2 
+#echo "zm_conv!"
+gcc  -c ../src/physics/cam/zm_conv_cuda.cpp -o zm_conv_cuda.o -O3  -Ofast || exit 2 
 echo "change this to icc for better opt"
-nvcc   -ccbin gcc-7  -c ../src/dynamics/fv/geopk_cuda.cu -o geopk_cuda.o  || exit 2
+#echo $INTEL
+icc -g -c  ../src/utils/fft_mkl.c  -o fft_mkl.o -fPIC -I $INTEL/mkl/include/|| exit 2 
+#echo "fft_mkl done ! "
--- models/atm/cam/src/physics/cam/aer_rad_props_cuda.cu
+++
@@ -1,5 +0,0 @@
-extern "C" 
-void get_hygro_rad_props_cuda_ ( int* pcols,int* pver, int* nswbands, int* ncol,int* krh,
-   double* wrh, double*  mass,double*  tau,double* tau_w,double* tau_w_g,double* tau_w_f){
-
-}
