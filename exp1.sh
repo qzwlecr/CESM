9409589a89e3be1ceec0e07d088e6ca708943d18
Lichen Zhang <qzwlecr@gmail.com>
2019-04-16 00:00:11 +0800

    port to cluster

--- exp1.sh
+++ exp1.sh
@@ -5,7 +5,9 @@ cd EXP1
 ./xmlchange -file env_run.xml -id DIN_LOC_ROOT -val /home/gpu_ubuntu/zhanglichen/asc2019/cesm/inputdata/inputdata_EXP1
 ./xmlchange -file env_run.xml -id DOCN_SOM_FILENAME -val pop_frc.gx1v6.091112.nc
 ./xmlchange -file env_run.xml -id STOP_N -val 5
-./xmlchange -file env_run.xml -id STOP_OPTION -val ndays
+./xmlchange -file env_run.xml -id STOP_OPTION -val nyears
+#./xmlchange -file env_run.xml -id REST_OPTION -val never
 ./cesm_setup || exit 2
+#./xmlchange -file env_run.xml -id PIO_TYPENAME -val pnetcdf
 ./EXP1.build || exit 3
 ./EXP1.run || exit 4
